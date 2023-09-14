64813deec598f7e15d6e74f2fa44b99a79fe9b48
Keco28 <kevin.correa1@udea.edu.co>
2023-09-13 22:22:23 -0500

    Lab1 first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Labs20232-Gr12"
+include(":Lab1-UI")
+ 
