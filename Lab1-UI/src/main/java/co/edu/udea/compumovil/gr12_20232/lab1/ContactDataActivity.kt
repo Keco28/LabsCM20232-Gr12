f49ed9d5c7ef563dc6ed402a8e5239cc259a8010
Jovan Alejandro Zambrano Bello <jovan.zambrano@udea.edu.co>
2023-09-15 19:31:13 -0500

    Anexo de la lista de paises

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr12_20232/lab1/ContactDataActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr12_20232/lab1/ContactDataActivity.kt
@@ -1,6 +1,7 @@
 package co.edu.udea.compumovil.gr12_20232.lab1
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,36 +12,37 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import co.edu.udea.compumovil.gr12_20232.lab1.ui.theme.Labs20232Gr12Theme
+import android.widget.AutoCompleteTextView;
+
 
 class ContactDataActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         setContent {
-            Labs20232Gr12Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting2("Android")
-                }
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.contact_data_layout)
+
+            var paises_latam = arrayOf(
+                "Bolivia", "Brasil", "Chile",
+                "Colombia", "Costa Rica", "Cuba", "Ecuador", "El Salvador",
+                "Guatemala", "Honduras", "México", "Nicaragua", "Panamá", "Paraguay",
+                "Perú", "Puerto Rico", "República Dominicana", "Uruguay")
+
+            var ciudades_colombia = arrayOf(
+                "Bogotá", "Medellín", "Cali", "Santa Marta",
+                "Barranquilla", "Villavicencio", "Bucaramanga", "Tunja"
+            )
+
+            val paises_adaptador = findViewById<AutoCompleteTextView>(R.id.textoPaises)
+
+            paises_adaptador.setOnClickListener {
+                paises_adaptador.requestFocus()
             }
+            var adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, paises_latam)
+            paises_adaptador.threshold = 0
+            paises_adaptador.setAdapter(adapter)
+
         }
     }
+
 }
 
-@Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview2() {
-    Labs20232Gr12Theme {
-        Greeting2("Android")
-    }
-}
